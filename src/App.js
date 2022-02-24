2b518fa295b23fd72e6705759b934751ea4aa1ef
fcribas <rakky26@gmail.com>
2022-02-23 21:24:34 -0500

    about me

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 // import Nav from './components/Nav';
-// import About from './components/About';
+import About from './components/About';
 // import Portfolio from './components/Portfolio';
 // import Contact from './components/Contact';
 
@@ -10,13 +10,16 @@ function App() {
     <div className="App">
       <header className="App-header">
         {/* possible image */}
-
         {/* EXAMPLE!!! */}
         {/* <img src={logo} className="App-logo" alt="logo" /> */}
-
-
+Portfolio
       </header>
 
+<main> 
+<About></About>
+
+
+</main>
     </div>
   );
 }
--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import coverImage from "../../assets/cover/milkyway.jpg"
 
 function About() {
   return (
     <section className="my-5"  >
+<h1 id="about">Who am I?</h1>
 
+<img src={coverImage} className="my-2" style={{ width: "100%" }} alt="cover" />
+
+<p> I am a person and I love to create and craft new things but I'm not the best at doing so. <br /> I'm at best average but what I do, it's always good.</p>
     </section>
   );
 }
